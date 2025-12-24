e17619b78e23400b9402da7540ed2434a1e51830
SudheerKondamuri <sudheersudheer7654321@gmail.com>
2025-12-24 18:31:36 +0530

    Deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,27 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
-import "forge-std/console2.sol";
-import "contracts/AuthorizationManager.sol";
-import "contracts/SecureVault.sol";
+import "forge-std/Script.sol";
+import "../contracts/SecureVault.sol";
+import "../contracts/AuthorizationManager.sol";
 
-contract CounterScript is Script {
-
-    function setUp() public {}
-
-    function run() public {
+contract DeploySystem is Script {
+    function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
+        address signer = vm.addr(deployerPrivateKey);
+
         vm.startBroadcast(deployerPrivateKey);
 
-        AuthorizationManager authManager = new AuthorizationManager(deployer);
+        AuthorizationManager auth = new AuthorizationManager(signer);
+        SecureVault vault = new SecureVault(address(auth));
 
-        SecureVault secureVault = new SecureVault(address(authManager));
+        console.log("AuthorizationManager deployed to:", address(auth));
+        console.log("SecureVault deployed to:", address(vault));
 
         vm.stopBroadcast();
-        console2.log("AuthorizationManager_Address:",address(authManager));
-        console2.log("SecureVault_Address:",address(secureVault));
-
     }
 }
